d5d22ef80d9fb2183544185a7842ccc63a0d373c
parigaur21 <149963898+parigaur21@users.noreply.github.com>
2026-07-22 13:00:19 +0530

    week 5 modified!

--- Week5/Angular/student-course-portal/src/app/guards/unsaved-changes.guard.ts
+++
@@ -1,5 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-
-export const unsavedChangesGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
-};
